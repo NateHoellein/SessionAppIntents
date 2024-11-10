0b40ab769e209af9f4675e32b9f4d7e007488d02
Nate <natehoellein@gmail.com>
2024-11-10 18:10:13 -0500

    Initial Commit

---
+++ SessionAppIntents/SessionAppIntentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionAppIntentsApp.swift
+//  SessionAppIntents
+//
+//  Created by nate on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SessionAppIntentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
